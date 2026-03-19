42c3747ba7c9260ebc013eea11613cb0ecd2cfd5
hengage <henrychizobaudeh@gmail.com>
2026-03-19 08:25:27 +0100

    perf(db): add database indexes to key model columns
    - Indexes resourceId in IdempotencyKey model
    - Indexes status, clientId, and contractorId in Contract model
    - Indexes contractId in Job model

--- src/domain/contracts/contract.model.ts
+++ src/domain/contracts/contract.model.ts
@@ -1,4 +1,4 @@
-import { Table, Column, Model, DataType, ForeignKey, BelongsTo, HasMany, Default } from 'sequelize-typescript';
+import { Table, Column, Model, DataType, ForeignKey, BelongsTo, HasMany, Default, Index } from 'sequelize-typescript';
 import { Profile } from '../profiles/profile.model';
 import { Job } from '../jobs/job.model';
 import { ContractStatus } from '../../constants/enums';
@@ -8,14 +8,17 @@ export class Contract extends Model {
   @Column(DataType.TEXT)
   declare terms: string;
 
+  @Index
   @Default(ContractStatus.NEW)
   @Column(DataType.ENUM(...Object.values(ContractStatus)))
   declare status: ContractStatus;
 
+  @Index
   @ForeignKey(() => Profile)
   @Column
   declare clientId: number;
 
+  @Index
   @ForeignKey(() => Profile)
   @Column
   declare contractorId: number;
